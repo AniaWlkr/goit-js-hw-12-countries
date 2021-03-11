e9dce13b92f67498c6edfbf3614ffe5d2c5b28f7
Ania Walker <ania.wlkr@gmail.com>
2021-03-11 21:11:45 +0200

    changed promises for async-await

--- src/index.js
+++ src/index.js
@@ -7,14 +7,21 @@ import onFetchError from './js/pnotify-error';
 
 const ref = getRefs();
 
-function querySubmit(event) { 
+async function querySubmit(event) { 
   const searchQuery = event.target.value.trim();
   console.log(searchQuery);
   if (!searchQuery) return;
   
-  api.fetchCountry(searchQuery)
-    .then(renderCountries)
-    .catch(onFetchError);
+  // api.fetchCountry(searchQuery)
+  //   .then(renderCountries)
+  //   .catch(onFetchError);
+  try { 
+    const countries = await api.fetchCountry(searchQuery);
+    renderCountries(countries);
+  }
+  catch (error) { 
+    onFetchError(error);
+  }
 }
 
 ref.queryInpRef.addEventListener('input', debounce(querySubmit, 500));
--- src/js/api-service.js
+++ src/js/api-service.js
@@ -1,8 +1,10 @@
 const BASE_URL = 'https://restcountries.eu/rest/v2/name/';
 
-function fetchCountry(countryName) {
-  return fetch(`${BASE_URL}${countryName}`)
-    .then(res => res.json());
+async function fetchCountry(countryName) {
+  // return fetch(`${BASE_URL}${countryName}`)
+  //   .then(res => res.json());
+  const resolve = await fetch(`${BASE_URL}${countryName}`);
+  return await resolve.json();
 }
 
 export default {fetchCountry};
